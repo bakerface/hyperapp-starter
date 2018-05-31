2988cb18d4a49d2464b33136f4632d762b279726
Chris Baker <mail.chris.baker@gmail.com>
2018-05-31 12:52:15 +0000

    remove elements.js
    Removing elements.js since it was not really necessary. This could
    eventually be useful again if we investigated using functional
    composition to compose view components. This would also have the added
    benefit of removing "h" from all of the views. Before doing this, we
    should consider what kind of performance hits this will incur.

--- src/components/elements.js
+++
@@ -1,6 +0,0 @@
-import { h } from "hyperapp";
-
-export const el = name => (props, children) =>
-  Array.isArray(props) ? h(name, {}, props) : h(name, props, children);
-
-export const div = el("div");

ecc57bf5160fa002f15757cbec62c6e73f4c773f
Chris Baker <mail.chris.baker@gmail.com>
2018-06-02 16:46:36 +0000

    remove head
    Remove the head element from the welcome page.

--- src/views/welcome.js
+++ src/views/welcome.js
@@ -1,8 +1,6 @@
 module.exports = () => h =>
   h("div", { id: "welcome" }, [
-    h("head", {}, [
-      h("title", {}, "Welcome"),
-      h("meta", { name: "description", content: "Welcome to hyperapp!" })
-    ]),
+    h("title", {}, "Welcome"),
+    h("meta", { name: "description", content: "Welcome to hyperapp!" }),
     h("h1", {}, "Welcome")
   ]);
